71281f74cbcb2a046dc404e84f3b804ef00aa284
Chris Marsh <cmhilfiker@gmail.com>
2020-07-27 10:23:06 +0000

    search implemented
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- products/views.py
+++ products/views.py
@@ -1,14 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import reverse, redirect, render, get_object_or_404
+from django.contrib import messages
 from .models import Product
-
+from django.db.models import Q
 
 # Create your views here.
+
+
 def all_products(request):
     """ A view to show all products and search queries"""
-    products = Product.objects.all
+
+    products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,
+                               "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
@@ -23,3 +40,4 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
