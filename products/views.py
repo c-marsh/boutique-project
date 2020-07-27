f8f35cc982e05f8026a98407683c518421ae3dda
Chris Marsh <cmhilfiker@gmail.com>
2020-07-27 12:26:41 +0000

    JS for filters
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import reverse, redirect, render, get_object_or_404
 from django.contrib import messages
 from .models import Product, Category
 from django.db.models import Q
+from django.db.models.functions import Lower
 
 # Create your views here.
 
