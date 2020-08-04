cbfcffc6d5abde5162245e7384638ad2e7bc7424
Chris Marsh <cmhilfiker@gmail.com>
2020-08-04 09:55:43 +0000

    fix heroku 400 error
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['cmh-boutique.heroku.app', 'local']
+ALLOWED_HOSTS = ['cmh-boutique.herokuapp.com', 'localhost']
 
 
 # Application definition
