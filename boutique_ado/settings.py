211818da32eaeb2d4c1b6598c5ecabedc660a05d
Chris Marsh <cmhilfiker@gmail.com>
2020-08-04 10:05:44 +0000

    Add cache control
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -175,6 +175,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
+# cache files
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
+
 
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'cmhboutique'
